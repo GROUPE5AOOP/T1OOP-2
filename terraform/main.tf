c98c8401ece63f4b007dcaa664b438fb3f214666
abdirahim888 <abdirahimmohamud97@gmail.com>
2025-11-14 16:59:48 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     local = {
-      source = "hashicorp/local"
+      source  = "hashicorp/local"
       version = "2.4.0"
     }
   }
@@ -9,7 +9,23 @@ terraform {
 
 provider "local" {}
 
-resource "local_file" "example" {
-  filename = "hello.txt"
+# Create multiple local files
+resource "local_file" "file1" {
+  filename = "file1.txt"
   content  = "Hello from Terraform running in Jenkins!"
+}
+
+resource "local_file" "file2" {
+  filename = "file2.txt"
+  content  = "Another file managed by Terraform"
+}
+
+resource "local_file" "file3" {
+  filename = "file3.txt"
+  content  = "Terraform can create many files locally"
+}
+
+# Output the list of files created
+output "file_list" {
+  value = [local_file.file1.filename, local_file.file2.filename, local_file.file3.filename]
 }
